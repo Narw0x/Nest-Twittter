044f20a1e342e5e0dfa1d0fa421318fdadfdeb8d
Martin Synák <martin.synak123@gmail.com>
2025-07-03 20:07:12 +0200

    ADD: env.example UPDATE: jwt secret fallback, env.example with default values, README.md REMOVE: prettier, env.template

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -3,14 +3,18 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private usersService: UsersService) {
+  constructor(
+    private usersService: UsersService,
+    private readonly configService: ConfigService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'ASdbuoit78243gudfssauao3753rt920fgaSdaus',
+      secretOrKey: configService.get<string>('JWT_SECRET') || '00000000000000000000000000000000',
     });
   }
 
